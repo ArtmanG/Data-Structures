68778b87fa7a200636ba7f876d43e4211a78041b
Gregory Hawman <g.hawman@gmail.com>
2020-07-14 13:32:46 -0700

    singly linked list test passed, fixed spacing and or indenting errors

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -1,5 +1,5 @@
 class Node:
-	    def __init__(self, value):
+    def __init__(self, value):
         self.value = value
         self.next = None
 
@@ -17,13 +17,12 @@ class LinkedList:
         self.head = None
         self.tail = None 
 
-        # def prepend(self, value):
-        #     current = self.head
-        #     self.head = Node(value, current)
+    # def prepend(self, value):
+    #     current = self.head
+    #     self.head = Node(value, current)
 
     def add_to_tail(self, value):
         new_node = Node(value)
-
         if self.head is None and self.tail is None:
             self.head = new_node
             self.tail = new_node
@@ -34,13 +33,11 @@ class LinkedList:
     def remove_head(self):
         if self.head is None and self.tail is None:
             return
-
         if not self.head.get_next():
             head = self.head
             self.head = None 
             self.tail = None 
             return head.get_value()
-
         val = self.head.get_value() 
         self.head = self.head.get_next()
         return val
@@ -48,17 +45,14 @@ class LinkedList:
     def remove_tail(self): 
         if self.head is None and self.tail is None:
             return
-
-		if self.head is self.tail:
-			value = self.head.get_value()
-			self.head = None
-			self.tail = None
-			return value
-
+        if self.head is self.tail:
+            value = self.head.get_value()
+            self.head = None
+            self.tail = None
+            return value
         current = self.head 
         while current.get_next() is not self.tail:
             current = current.get_next()
-
         val = self.tail.get_value() 
         self.tail = current
         return val
@@ -66,7 +60,6 @@ class LinkedList:
     def contains(self, value):
         if not self.head:
             return False
-
         current = self.head
         while current:
             if current.get_value() == value:
@@ -77,7 +70,6 @@ class LinkedList:
     def get_max(self):
         if not self.head:
             return None
-            
         max_value = self.head.get_value()
         current = self.head.get_next()
         while current:
